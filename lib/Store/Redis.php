ad8e9991500ba889fc87b68c0e82997be4d90fcc
Jacob Christiansen <jacob@colourbox.com>
2015-01-19 12:55:19 +0100

    Added default expiration time on all keys

--- lib/Store/Redis.php
+++ lib/Store/Redis.php
@@ -6,6 +6,7 @@ class sspmod_redis_Store_Redis extends SimpleSAML_Store
         $config      = SimpleSAML_Configuration::getConfig('module_redis.php');
         $host        = $config->getString('host', 'localhost');
         $this->redis = new Predis\Client($host);
+        $this->lifeTime = $globalConfig->getInteger('session.duration', 28800); // Default 8 hours
     }
 
     /**
@@ -26,6 +27,9 @@ class sspmod_redis_Store_Redis extends SimpleSAML_Store
     /**
      * Save a value to Redis
      *
+     * If no expiration time is given, then the expiration time is set to the
+     * session duration.
+     *
      * @param string $type     The datatype
      * @param string $key      The key
      * @param mixed $value     The value
@@ -36,9 +40,10 @@ class sspmod_redis_Store_Redis extends SimpleSAML_Store
         $redisKey = "simpleSAMLphp.$type.$key";
         $this->redis->set($redisKey, serialize($value));
 
-        if (!is_null($expire)) {
-            $this->redis->expire($redisKey, $expire);
+        if (is_null($expire)) {
+            $expire = $this->lifeTime;
         }
+        $this->redis->expire($redisKey, $expire);
     }
 
     /**
